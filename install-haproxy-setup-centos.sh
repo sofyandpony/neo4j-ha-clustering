bfe70c16ef78a1d2cf66c9a54551180bf00e1c3c
sofyandpony <dr.java85@gmail.com>
2018-07-12 16:15:13 +0800

    update neo4rj scripts

--- install-haproxy-setup-centos.sh
+++ install-haproxy-setup-centos.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 
 sudo yum install wget gcc pure-static pure-devel -y
 
@@ -19,7 +21,23 @@ sudo ln -s /usr/local/sbin/haproxy /usr/sbin/haproxy
 
 sudo mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.backup
 
+sudo ln -s /usr/local/sbin/haproxy /usr/sbin/haproxy
+
+sudo cp ~/haproxy-1.7.8/examples/haproxy.init /etc/init.d/haproxy
+sudo chmod 755 /etc/init.d/haproxy
+sudo systemctl daemon-reload
+
+sudo chkconfig haproxy on
+
+sudo useradd -r haproxy
+
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-port=2080/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=2088/tcp
+
+sudo firewall-cmd --reload
+
 sudo cp ./haproxy.cfg /etc/haproxy/
 
 
-sudo service haproxy start
+sudo systemctl restart haproxy
